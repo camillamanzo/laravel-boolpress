bbe1f951e857e89acc09b6d8a9329af41fb989da
camillamanzo <camillasouthafrica@gmail.com>
2021-11-15 17:31:00 +0100

    added carbon to post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     protected $fillable = ['title', 'date', 'author', 'content'];
+
+    public function getFormattedDate($column, $format = 'd-m-Y H:i:s' )
+    {
+        return Carbon::create($this->$column)->format($format);
+    }
 }
